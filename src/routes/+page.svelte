f3c9521106eb8652183d5a5c469b3edee2580b8a
Seth McNaughton <sdmcn43@gmail.com>
2026-05-13 19:05:00 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { invoke } from '@tauri-apps/api/core';
+  let result = $state('');
+</script>
+
+<button onclick={() => invoke<string>('greet', { name: 'World' }).then(r => result = r)}>
+  Call Rust
+</button>
+<p>{result}</p>
