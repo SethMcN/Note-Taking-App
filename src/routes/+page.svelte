b10ae991a18d0560bdb61ca70efb67650ce756bb
Seth McNaughton <sdmcn43@gmail.com>
2026-05-13 19:43:33 +0100

    Update button name parameter in greet function to 'Dave'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   let result = $state('');
 </script>
 
-<button onclick={() => invoke<string>('greet', { name: 'Bob' }).then(r => result = r)}>
+<button onclick={() => invoke<string>('greet', { name: 'Dave' }).then(r => result = r)}>
   Call Rust
 </button>
 <p>{result}</p>
