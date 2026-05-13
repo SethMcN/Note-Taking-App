b2aa85fa5398925126d4cb62ba3db7e0b2099d17
Seth McNaughton <sdmcn43@gmail.com>
2026-05-14 00:44:46 +0100

    intergrated Tiptap example code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
-  import { invoke } from '@tauri-apps/api/core';
-  let result = $state('');
+  // import { invoke } from '@tauri-apps/api/core';
+  import Tiptap from '$lib/Tiptap.svelte'
+
+
 </script>
 
-<button onclick={() => invoke<string>('greet', { name: 'Bob' }).then(r => result = r)}>
+<main>
+  <Tiptap />
+</main>
+
+
+
+
+<!-- <button onclick={() => invoke<string>('greet', { name: name }).then(r => result = r)}>
   Call Rust
 </button>
-<p>{result}</p>
+
+<input type="text" bind:value={name} placeholder="Enter your name" />
+
+<p>{result}</p> -->
