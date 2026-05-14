fa0a59da3381cf486c84ca8f168be4a4f65c70c9
Seth McNaughton <sdmcn43@gmail.com>
2026-05-14 01:36:42 +0100

    Swaped Tiptap for CodeMirror 6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,7 @@
 <script lang="ts">
-  // import { invoke } from '@tauri-apps/api/core';
-  import Tiptap from '$lib/Tiptap.svelte'
-
-
+	import Editor from '$lib/components/editor.svelte';
 </script>
 
 <main>
-  <Tiptap />
+	<Editor />
 </main>
-
-
-
-
-<!-- <button onclick={() => invoke<string>('greet', { name: name }).then(r => result = r)}>
-  Call Rust
-</button>
-
-<input type="text" bind:value={name} placeholder="Enter your name" />
-
-<p>{result}</p> -->
